523eb8ebaf99c797439ee967f7f12f49e2531c9c
KimJunpyo <bono1725@naver.com>
2023-03-30 00:13:42 +0900

    feat: 디자인 환경 조성, 불필요한 데이터 삭제
    1. 디자인을 위한 tailwind 환경을 설정하였습니다.
    2. 불필요한 데이터 및 사진들을 정리하였습니다.
    3. Route 방식을 적용하여 Pages의 파일에 접근하도록 하였습니다.

---
+++ src/Pages/Main.jsx
@@ -0,0 +1,15 @@
+import "../index.css";
+
+const Main = () => {
+  return (
+    <>
+      <main className="w-screen h-screen flex justify-center items-center">
+        <div className="bg-tahiti w-2/3 h-60 rounded-xl">
+          <div></div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Main;
